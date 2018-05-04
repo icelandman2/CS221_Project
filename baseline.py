5f91777f2934db08dabe6cd5772ede24e6a5ed5c
icelandman2 <icelandman2@gmail.com>
2018-05-03 21:00:41 -0700

    Control sequences and formatting

--- baseline.py
+++ baseline.py
@@ -1,23 +1,39 @@
 import sys
 import collections
+import re
 
 diseaseMap = {'cancer' : ['cough', 'runny nose', 'heart pain'], 'heart attack' : ['chest pain', 'shortness of breath']}
 
 def intro():
+    print(" ")
     print("Hello! My name is Dr. Gogol! I am an automated search assistant.")
     print("Tell me a bit about your symptoms, and I will tell you if I have a diagnosis.")
     print("Please keep in mind that this is for educational purposes only.")
     print("I am not an actual medical professional.")
     print("I am not intended to diagnose, cure, prevent, or treat any disease.")
-    print("Let's go!")
+    print("Please contact your doctor if you have any medical questions.")
+    print("With that in mind, let's go!")
+    print(" ")
 
 def wantToQuit():
+    print(" ")
+    print("Do you want to ask about another condition?")
+    decision = raw_input("Say yes if you want to, or press another key if you want to quit: ")
+    decision.strip(' ')
+    match = re.findall('^[Yy][Ee][Ss]', decision)
+    print(" ")
+    if len(match) > 0:
+        return False
+    print("See you soon!")
+    print(" ")
     return True
+    #return True
 
 def baseline(user_input):
     disCounts = collections.defaultdict(int)
     for disease in diseaseMap:
         listOfSymptoms = diseaseMap[disease]
+        listOfSymptoms = [x.lower() for x in listOfSymptoms]
         for symptom in user_input:
             if symptom.lower() in listOfSymptoms:
                 disCounts[disease] += 1
@@ -36,6 +52,7 @@ while(True):
     sympt = raw_input("What are your symptoms? ")
     sympt = sympt.split(',')
     sympt = [x.strip(' ') for x in sympt]
+    print(" ")
     print(baseline(sympt))
     if wantToQuit():
         break
