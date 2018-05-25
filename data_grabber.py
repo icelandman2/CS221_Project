711468d1af25bb6b0ae34972e41e9d45daa0f003
Ryan <ryangmui@gmail.com>
2018-05-25 13:53:08 -0700

    small disease data

--- data_grabber.py
+++ data_grabber.py
@@ -1,20 +1,25 @@
-rawParses = {}
 import sys
 import collections
 import re
 import urllib.request
+import os
 
 diseases = ['Anthrax', 'Chickenpox', 'Cholera', 'Schistosomiasis',
 			'Myocardial Infarction', 'Scabies', 'Influenza', 'Dengue Fever',
 			'Mumps virus', 'Rabies']
+
+rawParses = {}
 
 def getText():
     diseasesToCheck = ['_'.join(st.split(' ')) for st in diseases]
     for dis in diseasesToCheck:
         url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&titles=" + dis + "&redirects="
         contents = urllib.request.urlopen(url).read()
-        rawParses[' '.join(dis.split('_'))] = contents
-    print(len(rawParses))
+        rawParses[' '.join(dis.split('_'))] = str(contents)
 
 getText()
-print(rawParses)
+
+for disease in rawParses:
+	filepath = os.path.join('Disease_Data/', disease + ".txt")
+	f= open(filepath,"w+")
+	f.write(rawParses[disease])
