ab04cc37d89064574ab7c926679b1c9af540aa53
icelandman2 <icelandman2@gmail.com>
2018-05-26 02:59:32 -0700

    Pulls relations from raw data using hand-built features. Does not bootstrap new features yet

--- gogol.py
+++ gogol.py
@@ -2,7 +2,7 @@ import sys
 import collections
 import re
 import urllib.request
-
+import os
 '''
 Sentences pulled from the disease's wikipedia page that contain the word 'symptom'
 '''
@@ -27,6 +27,10 @@ symptomMap = {'Schistosomiasis' : schistosomiasis,
               'Influenza' : influenza,
               'Dengue Fever' : dengue_fever}
 
+rels = ['symptoms include (.*?)\.', 'symptoms are (.*?)\.', 'signs include (.*?)\.']
+
+symptsOfMapped = {}
+
 def intro():
     print(" ")
     print("Hello! My name is Dr. Gogol! I am an automated search assistant.")
@@ -38,6 +42,30 @@ def intro():
     print("With that in mind, let's go!")
     print(" ")
 
+def pull_relations():
+    for filename in os.listdir('Disease_Data/'):
+        diseaseName = " ".join(filename[:-4].split("_"))
+        filepath = "Disease_Data/" + filename
+        f = open(filepath, "r")
+        payload = f.readlines()
+
+        for rel in rels:
+            found = re.findall(rel, str(payload))
+            if len(found)>0:
+                rawSympts = found[0]
+                rawSympts = re.sub(' and ', ',', rawSympts)
+                symptsForMap = []
+                for s in rawSympts.split(','):
+                    s = s.strip()
+                    s = re.sub('[^a-zA-Z\s]', '', s)
+                    if len(s) > 1:
+                        symptsForMap.append(s)
+
+                if len(symptsForMap)>0:
+                    symptsOfMapped[diseaseName] = symptsForMap
+
+    #print(symptsOfMapped)
+        #print(payload)
 
 def wantToQuit():
     print(" ")
@@ -90,6 +118,7 @@ def info_from_prediction(prediction):
 
 sympt = ""
 intro()
+pull_relations()
 while(True):
     sympt = input("What are your symptoms? ")
     sympt = sympt.split(',')
